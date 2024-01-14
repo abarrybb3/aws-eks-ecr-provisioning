49d1565eebc6ace059388499f820bb0d8716b2b8
ablearning16 <ablearning16@gmail.com>
2024-01-13 20:44:15 -0500

    updated subnets

--- EKS-Cluster/variables.tf
+++ EKS-Cluster/variables.tf
@@ -1,11 +1,11 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-07d002bb7e32b67fc"
+  default = "subnet-082bb39a3c1e6cae3"
 }
 
 variable "subnet_id_2" {
   type = string
-  default = "subnet-03c49bbe3a085ef44"
+  default = "subnet-0ac3612eda33e03a1"
 }
 
 
