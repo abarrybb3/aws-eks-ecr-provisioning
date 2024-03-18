9bc343a7698722fb151a94981ec1735f5ba71f0f
abarrybb3 <abarrybb3@gmail.com>
2024-03-18 14:45:26 -0400

    updated node type

--- EKS-Cluster/main.tf
+++ EKS-Cluster/main.tf
@@ -108,12 +108,12 @@ resource "aws_eks_node_group" "worker-node-group" {
   node_group_name = "devops-workernodes"
   node_role_arn   = aws_iam_role.workernodes.arn
   subnet_ids      = [var.subnet_id_1, var.subnet_id_2]
-  instance_types = ["t3.micro"]
+  instance_types = ["t2.micro"]
 
     tags = {
       Name                        = "eks-node-group-instance-name"
     }
-    
+
   scaling_config {
     desired_size = 1
     max_size     = 1
